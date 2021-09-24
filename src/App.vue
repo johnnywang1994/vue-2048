89f5420328f3dff5e57454e2b02770fcd568c9c1
Johnny Wang <wangj@garena.com>
2021-09-24 11:54:04 +0800

    INIT

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Vue2048 ref="vue2048" @no-change="onNoChange" @point="onAddPoint" />
+  <p>Score: {{ totalPoint }}, Best Score: {{ bestPoint }}</p>
+  <h1>Vue Slide Puzzle</h1>
+
+  <!-- Control -->
+  <div class="panel">
+    <div class="reset-btn" @click="reset2048" touchstart="">
+      <span>Reset</span>
+    </div>
+  </div>
+
+  <!-- Social -->
+  <a
+    href="https://github.com/johnnywang1994/vue-2048/tree/master/src/components/Vue2048.vue"
+    target="_blank"
+  >
+    Source Code
+  </a>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref, watch } from 'vue'
+import Vue2048 from './components/Vue2048.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const vue2048 = ref(null);
+const totalPoint = ref(0);
+const bestPoint = ref(localStorage['best-score'] || 0);
+
+watch(totalPoint, (newPoint) => {
+  if (newPoint > bestPoint.value) {
+    bestPoint.value = newPoint;
+    localStorage['best-score'] = String(newPoint);
   }
+});
+
+function onNoChange() {
+  console.log('no change');
+}
+
+function onAddPoint(point) {
+  totalPoint.value += point;
+}
+
+function reset2048() {
+  totalPoint.value = 0;
+  vue2048.value.onResetGame();
 }
 </script>
 
 <style lang="scss">
+@mixin activeScale {
+  cursor: pointer;
+  transition: transform 0.2s, filter 0.2s;
+  &:active {
+    transform: scale(0.95);
+    filter: brightness(0.5);
+  }
+}
+
+@mixin noTouchOrSelect {
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  user-select: none;
+}
+
+@mixin btn_style {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 100px;
+  height: 40px;
+  font-weight: 700;
+  border-radius: 10px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +81,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.reset-btn {
+  color: #fff;
+  border: 1px solid rgb(0, 163, 128);
+  background: rgb(10, 206, 163);
+  @include btn_style;
+  @include activeScale;
+  @include noTouchOrSelect;
 }
 </style>
---
+++ src/components/Vue2048.vue
@@ -0,0 +1,323 @@
+<template>
+  <div class="playground">
+    <div v-for="(row, ri) in cardList" :key="`card-row-${ri}`" class="card-row">
+      <div v-for="(card, i) in row" :key="`card-${i}`" class="card">
+        <div
+          :class="['card-grid', { bounce: card.value > 0 }]"
+          :key="`card-grid-${i}-${card.value}`"
+          :data-value="card.value"
+        >
+          {{ card.value > 0 ? card.value : '' }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, watch } from 'vue';
+
+// -- Emits
+// eslint-disable-next-line
+const emit = defineEmits([
+  'no-change',
+  'point',
+]);
+
+// eslint-disable-next-line
+defineExpose({
+  onResetGame,
+})
+
+// -- Tools
+const cardChanged = ref(false);
+const [direction] = useTouchSlide('.playground');
+
+const getNextMap = {
+  1: (r, c) => () => r > 0 && cardList.value[--r][c],
+  2: (r, c) => () => cardList.value[r][++c],
+  3: (r, c) => () => r < 3 && cardList.value[++r][c],
+  4: (r, c) => () => cardList.value[r][--c],
+};
+
+const getLoopMap = {
+  1: onSlideTop,
+  2: onSlideRight,
+  3: onSlideDown,
+  4: onSlideLeft,
+};
+
+
+// -- Cards
+// [[], [], [], []]
+const cardList = ref([]);
+
+
+// -- Events
+function onAddCard(cardNumber, list) {
+  if (cardNumber <= 0) return;
+
+  const indexList = list || cardList.value
+    .reduce(
+      (list, row, i) => list.concat(row.map((card, j) => ({
+        i,
+        j,
+        ...card
+      }))),
+      [],
+    )
+    .filter((card) => card.value == 0);
+
+  if (cardNumber > indexList.length) return;
+
+  const index = Math.floor(Math.random() * indexList.length);
+  const target = cardList.value[indexList[index].i][indexList[index].j];
+  target.value = 2;
+  indexList.splice(index, 1);
+
+  cardChanged.value = true;
+
+  return onAddCard(--cardNumber, indexList);
+}
+
+function onMove(direction, r, c) {
+  let next;
+  let card = cardList.value[r][c];
+  // card exist
+  if (card.value > 0) {
+    const getNext = getNextMap[direction](r, c);
+    next = getNext();
+    // move
+    while (next && next.value == 0) {
+      next.value = card.value;
+      card.value = 0;
+      card = next;
+      next = getNext();
+      cardChanged.value = true;
+    }
+    // merge
+    if (next && next.value === card.value) {
+      next.value *= 2;
+      card.value = 0;
+      cardChanged.value = true;
+      // add value
+      emit('point', next.value);
+    }
+  }
+}
+
+function onSlideRight(direction) {
+  for (let r = 0; r < 4; r++) {
+    for (let c = 3; c >= 0; c--) {
+      onMove(direction, r, c);
+    }
+  }
+}
+
+function onSlideLeft(direction) {
+  for (let r = 0; r < 4; r++) {
+    for (let c = 0; c < 4; c++) {
+      onMove(direction, r, c);
+    }
+  }
+}
+
+function onSlideTop(direction) {
+  for (let c = 0; c < 4; c++) {
+    for (let r = 0; r < 4; r++) {
+      onMove(direction, r, c);
+    }
+  }
+}
+
+function onSlideDown(direction) {
+  for (let c = 0; c < 4; c++) {
+    for (let r = 3; r >= 0; r--) {
+      onMove(direction, r, c);
+    }
+  }
+}
+
+function onResetGame() {
+  cardList.value = Array(4).fill('').map(
+    () => Array(4).fill('').map(() => ({
+      value: 0
+    }))
+  );
+  onAddCard(2);
+}
+
+watch(direction, (newVal) => {
+  const loopFn = getLoopMap[newVal];
+  if (loopFn) {
+    loopFn(newVal);
+    onAddCard(1);
+    if (!cardChanged.value) {
+      emit('no-change');
+    }
+    cardChanged.value = false;
+  }
+})
+
+// -- Init
+onResetGame();
+
+
+function useTouchSlide(selector, safeOffset = 20) {
+  // 1: top, 2: right, 3: down, 4: left
+  const direction = ref(0);
+  
+  let xDown = null;
+  let yDown = null;
+  
+  function handleTouchStart(evt) {
+    const firstTouch = (evt.touches && evt.touches[0]) || evt;
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+  }
+  
+  function handleTouchStop() {
+    xDown = yDown = null;
+    direction.value = 0;
+  }
+  
+  function handleTouchMove(evt) {
+    if (!xDown || !yDown) return;
+    
+    const currentTouch = (evt.touches && evt.touches[0]) || evt;
+    const xUp = currentTouch.clientX;
+    const yUp = currentTouch.clientY;
+    
+    const xDiff = xDown - xUp;
+    const yDiff = yDown - yUp;
+    
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+      if (Math.abs(xDiff) < safeOffset) return;
+      direction.value = xDiff > 0 ? 4 : 2;
+    } else {
+      if (Math.abs(yDiff) < safeOffset) return;
+      direction.value = yDiff > 0 ? 1 : 3;
+    }
+  }
+  
+  function init() {
+    const target = document.querySelector(selector);
+    target.addEventListener('touchstart', handleTouchStart, false);
+    target.addEventListener('touchmove', handleTouchMove, false);
+    target.addEventListener('touchend', handleTouchStop, false);
+    target.addEventListener('mousedown', handleTouchStart, false);
+    target.addEventListener('mousemove', handleTouchMove, false);
+    target.addEventListener('mouseup', handleTouchStop, false);
+    target.addEventListener('mouseleave', handleTouchStop, false);
+  }
+  
+  function destroy() {
+    const target = document.querySelector(selector);
+    target.removeEventListener('touchstart', handleTouchStart);
+    target.removeEventListener('touchmove', handleTouchMove);
+    target.removeEventListener('touchend', handleTouchStop);
+    target.removeEventListener('mousedown', handleTouchStart);
+    target.removeEventListener('mousemove', handleTouchMove);
+    target.removeEventListener('mouseup', handleTouchStop);
+  }
+  
+  onMounted(() => {
+    init();
+  })
+  
+  return [direction, destroy];
+}
+</script>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+
+@keyframes bounce {
+  0% {
+    transform: scale(1);
+  }
+  70% {
+    transform: scale(1.1);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+@function pow($base, $exponents) {
+  @if $exponents == 0 {
+    @return 0;
+  }
+  
+  $raised: 1;
+
+  @for $i from 1 through $exponents {
+    $raised: $raised * $base;
+  }
+
+  @return $raised;
+}
+
+$bgcolor-array: (
+  rgba(238, 228, 218, 0.35),
+  #faf8ef,
+  #eee1c9,
+  #f3b27a,
+  #f69664,
+  #f77c5f,
+  #f75f3b,
+  #edd073,
+  #edcc62,
+  #edc950,
+  #edc53f,
+  #edc22e, // 2048
+);
+
+.playground {
+  display: flex;
+  flex-wrap: wrap;
+  width: 300px;
+  height: 300px;
+  margin: auto;
+  background: #bbada0;
+  user-select: none;
+  cursor: grab;
+  > .card-row {
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    height: 25%;
+    > .card {
+      width: 25%;
+      height: 100%;
+      font-size: 30px;
+      font-weight: bold;
+      padding: 5px;
+      > .card-grid {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        color: #776e65;
+        &.bounce {
+          animation: bounce 0.2s ease-in 1;
+        }
+        @for $i from 1 through length($bgcolor-array) {
+          &[data-value='#{pow(2, $i - 1)}'] {
+            background: nth($bgcolor-array, $i);
+            @if pow(2, $i - 1) >= 32 {
+              color: #f9f6f2;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
